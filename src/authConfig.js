5685765e23792defc6ff96d6cc8585e6736f5f6d
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-03-02 18:32:02 -0600

    adding env.local to gitignore

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,6 +1,6 @@
 export const msalConfig = {
     auth: {
-      clientId: '27c33eb6-5f97-4ca2-8bd5-e6e12a2f0455', 
+      clientId: process.env.NEXT_PUBLIC_AUTHCLIENTID, 
       authority: 'https://favolog.b2clogin.com/favolog.onmicrosoft.com/B2C_1A_signup_signin',
       knownAuthorities: ['favolog.b2clogin.com'],
       redirectUri: process.env.NEXT_PUBLIC_REDIRECTURI,
