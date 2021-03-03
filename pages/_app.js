1c4296c4bace8f5a4905ae52acb1e3345e672dae
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-03-03 16:54:59 -0600

    fixed issue with context

--- pages/_app.js
+++ pages/_app.js
@@ -2,59 +2,19 @@ import { MsalProvider } from "@azure/msal-react"
 import { PublicClientApplication } from "@azure/msal-browser"
 import { msalConfig } from "../src/authConfig"
 import Layout from '../components/Layout/Layout'
-import {useState} from 'react'
-import { useRouter } from 'next/router'
-import {UserContext} from '../src/UserContext'
+import {UserContextProvider} from '../src/UserContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {  
-  const msalInstance = new PublicClientApplication(msalConfig)  
-  const [user, setUser] = useState()  
-  const router = useRouter()
-
-  const postUser = async (authResult) => {        
-    const claims = authResult.idTokenClaims    
-
-    const user = {
-        emailAddress: claims.emails[0],
-        firstName: claims.given_name,
-        lastName: claims.family_name,                
-        externalId: claims.sub
-    }   
-    
-    fetch(`${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/user`, {
-        method: "POST",
-        headers: {            
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    })
-      .then(response => response.json())
-      .then(data => setUser(data))
-  }
-  
-  const signIn = () => {
-    msalInstance.loginPopup()
-      .then((result)=> {
-          postUser(result)
-      })
-      .catch((error)=>{console.error('error:', error)})
-  }
-
-  const signOut = () => {    
-    msalInstance.logout()
-    .then(()=> setUser())
-    .then(()=> router.push(`/`))
-  }
-      
+  const msalInstance = new PublicClientApplication(msalConfig)    
+       
   return (
     <MsalProvider instance={msalInstance}>
-      <UserContext.Provider value={{user, signIn, signOut, setUser}}>
+      <UserContextProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-      </UserContext.Provider>
+      </UserContextProvider>
     </MsalProvider>    
   )
 }
--- src/UserContext.js
+++ src/UserContext.js
@@ -1,12 +1,55 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
+import {useMsal} from '@azure/msal-react'
 
 export const UserContext = createContext()
 
-export const UserContextProvider = () => {  
-    const [user, setUser] = useState()
+export const UserContextProvider = ({ children }) => {       
+    const {instance, accounts} = useMsal()
+    const [ user, setUser ] = useState()
 
+    const postUser = async (authResult) => {        
+        const claims = authResult.idTokenClaims    
+    
+        const user = {
+            emailAddress: claims.emails[0],
+            firstName: claims.given_name,
+            lastName: claims.family_name,                
+            externalId: claims.sub
+        }   
+        
+        fetch(`${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/user`, {
+            method: "POST",
+            headers: {            
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+          .then(response => response.json())
+          .then(data => setUser(data))
+      }
+
+    const signIn = () => {
+        instance.loginPopup()
+          .then((result)=> {
+              postUser(result)
+          })
+          .catch((error)=>{console.error('error:', error)})
+      }
+      
+      const signOut = () => {    
+        instance.logout()
+        .then(()=> setUser())
+        .then(()=> router.push(`/`))
+      }
+
+      useEffect(()=>{                
+        if (accounts.length > 0)
+            postUser(accounts[0])
+      }, [accounts])
+      
     return (
-    <UserContext.Provider value={{user}}>
+    <UserContext.Provider value={{user, setUser, signIn, signOut}}>
         {children}
     </UserContext.Provider>
     )    
