1a47467b0d3c111b6b1ea633d27addf351659eec
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-03-04 10:06:11 -0600

    added guest feed

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,30 @@ import styles from '../styles/CatalogStyles.module.css'
 import { useContext, useEffect, useState } from 'react'
 import {UserContext} from '../src/UserContext'
 import FeedItemCard from '../components/FeedItemCard'
+import {useIsAuthenticated} from '@azure/msal-react'
 
 export default function Page() {  
-    const {user} = useContext(UserContext)    
-    const [feedItems, setFeedItems] = useState([])
-    
-    useEffect(()=>{
-        if (user){
-            fetch(`${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/user/${user.username}/feed`)
-                .then(response => response.json())
-                .then(data => setFeedItems(data))
-        }        
-    }, [user])
+  const isAuthenticated = useIsAuthenticated()
+  
+  const {user} = useContext(UserContext)    
+  const [feedItems, setFeedItems] = useState([])
+  
+  useEffect(()=>{      
+      if (isAuthenticated && user){
+          fetch(`${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/feed/user/${user.username}`)
+              .then(response => response.json())
+              .then(data => setFeedItems(data))
+      } else{
+        fetch(`${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/feed`)
+              .then(response => response.json())
+              .then(data => setFeedItems(data))
+      }      
+  }, [user, isAuthenticated])
 
-    return <div className={styles.catalog}>        
-      {feedItems && feedItems.map((item) => (        
-        <FeedItemCard key={item.id} item={item}/>
-      ))}    
-    </div>  
+  return <div className={styles.catalog}>        
+    {feedItems && feedItems.map((item) => (        
+      <FeedItemCard key={item.id} item={item}/>
+    ))}    
+  </div>
 }
 
