7f1134f76dec0003d11d1a983fdec4bb1fba6bc9
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-03-11 13:33:11 -0600

    fixed issue on search page

--- pages/search.js
+++ pages/search.js
@@ -1,42 +1,42 @@
 import Link from 'next/link'
-import UserCard from '../components/UserCard'
+import UserCard from '../components/user/UserCard'
 import styles from '../styles/CatalogStyles.module.css'
 
-export default function Page({searchQuery, results}) {  
-
-  return <div className={styles.searchResults}> 
-      <span>Search results <b>'{searchQuery}'</b></span>
+export default function Page({ searchQuery, results }) {
+  return (
+    <div className={styles.searchResults}>
+      <span>
+        Search results <b>'{searchQuery}'</b>
+      </span>
       <h2>Users</h2>
       <div className={styles.catalog}>
         {results.users.map((item) => (
-          <UserCard key={item.id} user={item}/>
-        ))}      
+          <UserCard key={item.id} user={item} />
+        ))}
       </div>
-      <h2>Catalogs</h2>      
+      <h2>Catalogs</h2>
       <div className={styles.catalog}>
         {results.catalogs.map((item) => (
-          <Link href={`/catalog/${item.id}`} key={item.id}><span>{item.Name}</span></Link>
-        ))}   
-      </div>    
-      <h2>Items</h2>      
-      <div className={styles.catalog}>
-        {results.items.map((item) => (
-          <Link href={`/item/${item.id}`} key={item.id}><span>{item.title}</span></Link>  
-        ))}       
-      </div>       
+          <Link href={`/catalog/${item.id}`} key={item.id}>
+            <span>{item.Name}</span>
+          </Link>
+        ))}
+      </div>
     </div>
- }
+  )
+}
 
-export async function getServerSideProps({query}) {
+export async function getServerSideProps({ query }) {
   const searchQuery = query.q
-  const res = await fetch(`${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/Search?query=${searchQuery}`)  
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/Search?query=${searchQuery}`
+  )
   const results = await res.json()
-  
+
   return {
     props: {
       searchQuery,
-      results
-    }
+      results,
+    },
   }
 }
-
