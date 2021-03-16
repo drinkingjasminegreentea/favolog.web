4460dfc0a2da9139f569f18d9553555c2c3e94db
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-03-16 13:05:59 -0500

    using swr on index and explore pages

--- pages/explore.js
+++ pages/explore.js
@@ -1,24 +1,46 @@
 import styles from '../styles/CatalogStyles.module.css'
-import { useEffect, useState, useContext } from 'react'
+import { useEffect, useContext } from 'react'
 import FeedItemCard from '../components/item/FeedItemCard'
 import { ActivePages, PageContext } from '../src/PageContext'
+import useSWR from 'swr'
 
 export default function Page() {
-  const [feedItems, setFeedItems] = useState([])
   const { setActivePage } = useContext(PageContext)
 
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/feed`)
-      .then((response) => response.json())
-      .then((data) => setFeedItems(data))
     setActivePage(ActivePages.explore)
   }, [])
 
+  const fetcher = (url) => {
+    return fetch(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => {
+        if (response.ok) return response.json()
+        return Promise.reject(response)
+      })
+      .catch((error) => {
+        console.log('Something went wrong.', error)
+      })
+  }
+
+  const { data, error } = useSWR(
+    `${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/feed/`,
+    fetcher
+  )
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
   return (
-    <div className={styles.catalog}>
-      {feedItems.map((item) => (
-        <FeedItemCard key={item.id} item={item} />
-      ))}
-    </div>
+    <>
+      <div className={styles.catalog}>
+        {data.items.map((item) => (
+          <FeedItemCard key={item.id} item={item} />
+        ))}
+      </div>
+    </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,18 @@
 import styles from '../styles/CatalogStyles.module.css'
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect } from 'react'
 import { UserContext } from '../src/UserContext'
 import { ActivePages, PageContext } from '../src/PageContext'
-import { useMsal, useIsAuthenticated } from '@azure/msal-react'
-import FeedItemCard from '../components/item/FeedItemCard'
 import Alert from 'react-bootstrap/Alert'
+import FeedItemCard from '../components/item/FeedItemCard'
+import useSWR from 'swr'
 
 export default function Page() {
-  const { accounts } = useMsal()
-  const { isAuthenticated } = useIsAuthenticated()
   const { user } = useContext(UserContext)
   const { setActivePage } = useContext(PageContext)
-  const [feedItems, setFeedItems] = useState([])
-  const [newUser, setNewUser] = useState(false)
-  const [guestUser, setGuestUser] = useState(false)
   const { acquireToken } = useContext(UserContext)
 
-  const fetchDiscoverFeed = () => {
-    fetch(`${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/feed`, {
+  const fetchDiscoverFeed = (url) => {
+    return fetch(url, {
       method: 'GET',
       headers: {
         Accept: 'application/json',
@@ -27,62 +22,51 @@ export default function Page() {
         if (response.ok) return response.json()
         return Promise.reject(response)
       })
-      .then((data) => {
-        setFeedItems(data)
-      })
       .catch((error) => {
         console.log('Something went wrong.', error)
       })
-    setActivePage(ActivePages.home)
+  }
+
+  const fetchUserFeed = (url) => {
+    return acquireToken().then((accessToken) => {
+      return fetch(url, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+        .then((response) => {
+          if (response.ok) return response.json()
+          return Promise.reject(response)
+        })
+        .catch((error) => {
+          console.log('Something went wrong.', error)
+        })
+    })
+  }
+  let url = null
+  let fetcher = null
+
+  if (user) {
+    url = `${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/feed/user/${user.id}`
+    fetcher = fetchUserFeed
+  } else {
+    url = `${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/feed/`
+    fetcher = fetchDiscoverFeed
   }
 
   useEffect(() => {
-    if (accounts && accounts.length == 0) {
-      fetchDiscoverFeed()
-      setGuestUser(true)
-      setNewUser(false)
-    }
-  }, [accounts])
-
-  useEffect(() => {
-    if (user) {
-      acquireToken().then((accessToken) => {
-        fetch(
-          `${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/feed/user/${user.id}`,
-          {
-            method: 'GET',
-            headers: {
-              Accept: 'application/json',
-              Authorization: `Bearer ${accessToken}`,
-            },
-          }
-        )
-          .then((response) => {
-            if (response.ok) return response.json()
-            return Promise.reject(response)
-          })
-          .then((data) => {
-            if (data.length > 0) {
-              setFeedItems(data)
-              setNewUser(false)
-              setGuestUser(false)
-            } else {
-              setNewUser(true)
-              setGuestUser(false)
-              fetchDiscoverFeed()
-            }
-          })
-          .catch((error) => {
-            console.log('Something went wrong.', error)
-          })
-      })
-    }
     setActivePage(ActivePages.home)
-  }, [user])
+  })
 
+  const { data, error } = useSWR(url, fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
   return (
     <>
-      {newUser && (
+      {data.newUser && (
         <Alert variant='info'>
           <Alert.Heading>Welcome!</Alert.Heading>
           <span>
@@ -91,7 +75,7 @@ export default function Page() {
           </span>
         </Alert>
       )}
-      {guestUser && (
+      {data.guestUser && (
         <Alert variant='info'>
           <Alert.Heading>Welcome!</Alert.Heading>
           <span>
@@ -100,7 +84,7 @@ export default function Page() {
         </Alert>
       )}
       <div className={styles.catalog}>
-        {feedItems.map((item) => (
+        {data.items.map((item) => (
           <FeedItemCard key={item.id} item={item} />
         ))}
       </div>
