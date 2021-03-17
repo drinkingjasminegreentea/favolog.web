74a1950ea79f8efb68df9f74d17defb32ce48b6e
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-03-17 15:27:01 -0500

    useSWR for the user page

--- pages/[username].js
+++ pages/[username].js
@@ -3,26 +3,46 @@ import CatalogCard from '../components/catalog/CatalogCard'
 import ProfileInfo from '../components/user/ProfileInfo'
 import { PageContext } from '../src/PageContext'
 import { useContext, useEffect } from 'react'
+import useSWR from 'swr'
 
-export default function Page({ userProfile }) {
-  const user = userProfile.user
-  const catalogs = userProfile.catalogs
+export default function Page({ username }) {
   const { setActivePage } = useContext(PageContext)
 
   useEffect(() => {
     setActivePage(null)
   }, [])
 
+  const fetcher = (url) => {
+    return fetch(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => {
+        if (response.ok) return response.json()
+        return Promise.reject(response)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+  const url = `${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/user/${username}/profile`
+  const { data, error } = useSWR(url, fetcher)
+
+  if (error) return <div>failed to load </div>
+  if (!data) return <div>loading...</div>
+
   return (
     <>
       <ProfileInfo
-        user={user}
-        totalFollowing={userProfile.totalFollowing}
-        totalFollowers={userProfile.totalFollowers}
+        user={data.user}
+        totalFollowing={data.totalFollowing}
+        totalFollowers={data.totalFollowers}
       />
       <div className={styles.catalog}>
-        {catalogs &&
-          catalogs.map((catalog) => (
+        {data.catalogs &&
+          data.catalogs.map((catalog) => (
             <CatalogCard key={catalog.id} catalog={catalog} />
           ))}
       </div>
@@ -31,12 +51,9 @@ export default function Page({ userProfile }) {
 }
 
 export async function getServerSideProps({ params }) {
-  const userProfile = await fetch(
-    `${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/user/${params.username}/profile`
-  ).then((response) => response.json())
   return {
     props: {
-      userProfile,
+      username: params.username,
     },
   }
 }
