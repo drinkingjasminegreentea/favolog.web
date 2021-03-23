87e89bbbdcd41fa361a6e50a6beda147de90f470
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-03-23 08:29:05 -0500

    added pagination

--- pages/explore.js
+++ pages/explore.js
@@ -1,12 +1,14 @@
 import styles from '../styles/CatalogStyles.module.css'
-import { useEffect, useContext } from 'react'
+import { useEffect, useContext, useState } from 'react'
 import FeedItemCard from '../components/item/FeedItemCard'
 import { ActivePages, PageContext } from '../src/PageContext'
 import useSWR from 'swr'
 import Spinner from 'react-bootstrap/Spinner'
+import Button from 'react-bootstrap/Button'
 
 export default function Page() {
   const { setActivePage } = useContext(PageContext)
+  const [pageIndex, setPageIndex] = useState(1)
 
   useEffect(() => {
     setActivePage(ActivePages.explore)
@@ -20,7 +22,10 @@ export default function Page() {
       },
     })
       .then((response) => {
-        if (response.ok) return response.json()
+        if (response.ok) {
+          window.scrollTo(0, 0)
+          return response.json()
+        }
         return Promise.reject(response)
       })
       .catch((error) => {
@@ -29,19 +34,32 @@ export default function Page() {
   }
 
   const { data, error } = useSWR(
-    `${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/feed/`,
+    `${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/feed?pageIndex=${pageIndex}`,
     fetcher
   )
+
+  const loadMore = () => {
+    setPageIndex(pageIndex + 1)
+  }
 
   if (error) return <div>failed to load</div>
   if (!data) return <Spinner className={styles.loading} animation='grow' />
   return (
     <>
       <div className={styles.catalog}>
-        {data.items.map((item) => (
+        {data.page.items.map((item) => (
           <FeedItemCard key={item.id} item={item} />
         ))}
       </div>
+      {data.page.hasNextPage && (
+        <Button
+          variant='secondary'
+          className={styles.loadMore}
+          onClick={loadMore}
+        >
+          Load more
+        </Button>
+      )}
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import styles from '../styles/CatalogStyles.module.css'
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../src/UserContext'
 import { ActivePages, PageContext } from '../src/PageContext'
 import Alert from 'react-bootstrap/Alert'
 import FeedItemCard from '../components/item/FeedItemCard'
 import useSWR from 'swr'
 import Spinner from 'react-bootstrap/Spinner'
+import Button from 'react-bootstrap/Button'
 
 export default function Page() {
   const { setActivePage } = useContext(PageContext)
   const { user, acquireToken } = useContext(UserContext)
+  const [pageIndex, setPageIndex] = useState(1)
 
   useEffect(() => {
     setActivePage(ActivePages.home)
@@ -23,7 +25,10 @@ export default function Page() {
       },
     })
       .then((response) => {
-        if (response.ok) return response.json()
+        if (response.ok) {
+          window.scrollTo(0, 0)
+          return response.json()
+        }
         return Promise.reject(response)
       })
       .catch((error) => {
@@ -41,7 +46,10 @@ export default function Page() {
         },
       })
         .then((response) => {
-          if (response.ok) return response.json()
+          if (response.ok) {
+            window.scrollTo(0, 0)
+            return response.json()
+          }
           return Promise.reject(response)
         })
         .catch((error) => {
@@ -53,14 +61,18 @@ export default function Page() {
   let fetcher = null
 
   if (user) {
-    url = `${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/feed/user/${user.id}`
+    url = `${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/feed/user/${user.id}?pageIndex=${pageIndex}`
     fetcher = fetchUserFeed
   } else {
-    url = `${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/feed/`
+    url = `${process.env.NEXT_PUBLIC_FAVOLOGAPIBASEURL}/feed/?pageIndex=${pageIndex}`
     fetcher = fetchDiscoverFeed
   }
 
   const { data, error } = useSWR(url, fetcher)
+
+  const loadMore = () => {
+    setPageIndex(pageIndex + 1)
+  }
 
   if (error) return <div>failed to load</div>
   if (!data) return <Spinner className={styles.loading} animation='grow' />
@@ -68,27 +80,37 @@ export default function Page() {
     <>
       {data.newUser && (
         <Alert variant='info'>
-          <Alert.Heading>Welcome!</Alert.Heading>
           <span>
             Add your favorite item to a catalog by clicking on the plus button.
             Discover, search and find people to follow. Here are few of the
-            latest added favorites.
+            latest added favorites by our users.
           </span>
         </Alert>
       )}
       {data.guestUser && (
         <Alert variant='info'>
-          <Alert.Heading>Welcome!</Alert.Heading>
           <span>
-            Sign in to start creating and sharing your favorites catalogs.
+            Favolog is a place where you can catalog all your favorite things in
+            the world, which may include items such as products, places, books
+            or movies and share them with your followers. Sign in to start
+            creating, sharing and discovering.
           </span>
         </Alert>
       )}
       <div className={styles.catalog}>
-        {data.items.map((item) => (
+        {data.page.items.map((item) => (
           <FeedItemCard key={item.id} item={item} />
         ))}
       </div>
+      {user && data.page.hasNextPage && (
+        <Button
+          variant='secondary'
+          className={styles.loadMore}
+          onClick={loadMore}
+        >
+          Load more
+        </Button>
+      )}
     </>
   )
 }
