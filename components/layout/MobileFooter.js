40b7ddea54bd3f94f107a5a90aabbda3738d3145
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-04-14 12:43:26 -0500

    activate menu on click

--- components/layout/MobileFooter.js
+++ components/layout/MobileFooter.js
@@ -1,40 +1,80 @@
 import styles from '../../styles/MobileFooter.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../../src/AuthContext'
 
 export default function MobileFooter() {
   const { currentUser } = useContext(AuthContext)
+  const [active, setActive] = useState('')
+  const [homeImage, setHomeImage] = useState('')
+  const [searchImage, setSearchImage] = useState('')
+  const [addImage, setAddImage] = useState('')
+  const [profileImage, setProfileImage] = useState('')
+
+  useEffect(() => {
+    setActive('home')
+  }, [])
+
+  useEffect(() => {
+    active == 'home'
+      ? setHomeImage('home-active.svg')
+      : setHomeImage('home.svg')
+    active == 'search'
+      ? setSearchImage('search-active.svg')
+      : setSearchImage('search.svg')
+    active == 'add' ? setAddImage('plus-active.svg') : setAddImage('plus.svg')
+    active == 'profile'
+      ? setProfileImage('person-active.svg')
+      : setProfileImage('person.svg')
+  }, [active])
+
+  const clickHandler = (e) => {
+    setActive(e.target.id)
+  }
 
   return (
     <div className={styles.footerMenu}>
       <Link href='/'>
-        <div className='button'>
-          <Image src='/icons/home.svg' width='20' height='20' layout='fixed' />
-        </div>
-      </Link>
-      <Link href='/'>
-        <div className='button'>
+        <div id='home' className='button' onClick={clickHandler}>
           <Image
-            src='/icons/search.svg'
-            width='23'
-            height='23'
+            id='home'
+            src={`/icons/${homeImage}`}
+            width='20'
+            height='20'
             layout='fixed'
           />
         </div>
       </Link>
       <Link href='/'>
-        <div className='button'>
-          <Image src='/icons/plus.svg' width='23' height='23' layout='fixed' />
+        <div id='search' className='button' onClick={clickHandler}>
+          <Image
+            id='search'
+            src={`/icons/${searchImage}`}
+            width='25'
+            height='25'
+            layout='fixed'
+          />
+        </div>
+      </Link>
+      <Link href='/'>
+        <div id='add' className='button' onClick={clickHandler}>
+          <Image
+            id='add'
+            src={`/icons/${addImage}`}
+            width='25'
+            height='25'
+            layout='fixed'
+          />
         </div>
       </Link>
       <Link href={`/${currentUser.displayName}`}>
-        <div className='button'>
+        <div id='profile' className='button' onClick={clickHandler}>
           <Image
-            src='/icons/person.svg'
-            width='23'
-            height='23'
+            id='profile'
+            src={`/icons/${profileImage}`}
+            width='25'
+            height='25'
             layout='fixed'
           />
         </div>
