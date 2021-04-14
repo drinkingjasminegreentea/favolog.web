c89f227304ec1ecdf4116ef43e8bd1e1d5624450
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-04-13 21:57:06 -0500

    i hate regex

--- pages/api/openGraph/[...slug].js
+++ pages/api/openGraph/[...slug].js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const { slug } = req.query
 
   let url = encodeURIComponent(slug)
-  url = url.replace('/%2C/g', '%2F')
+  url = url.replace(/%2C/g, '%2F')
 
   const linkPreviewUrl = `${process.env.LINK_PREVIEW_API_URL}${url}`
   const openGraphUrl = `${process.env.OPEN_GRAPH_API_URL}${url}?app_id=${process.env.OPEN_GRAPH_API_KEY}`
