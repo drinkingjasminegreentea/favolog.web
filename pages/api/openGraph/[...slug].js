614257591aac7410d35978316335dd11abd803de
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-04-13 21:13:02 -0500

    why

--- pages/api/openGraph/[...slug].js
+++ pages/api/openGraph/[...slug].js
@@ -1,13 +1,9 @@
 export default async function handler(req, res) {
   const { slug } = req.query
 
-  const linkPreviewUrl = `${
-    process.env.LINK_PREVIEW_API_URL
-  }${encodeURIComponent(slug)}`
-
-  const openGraphUrl = `${process.env.OPEN_GRAPH_API_URL}${encodeURIComponent(
-    slug
-  )}?app_id=${process.env.OPEN_GRAPH_API_KEY}`
+  const url = encodeURIComponent(slug)
+  const linkPreviewUrl = `${process.env.LINK_PREVIEW_API_URL}${url}`
+  const openGraphUrl = `${process.env.OPEN_GRAPH_API_URL}${url}?app_id=${process.env.OPEN_GRAPH_API_KEY}`
 
   console.log({ linkPreviewUrl })
   console.log({ openGraphUrl })
@@ -15,13 +11,13 @@ export default async function handler(req, res) {
   // try link preview first, then open graph io
   let response = await fetch(linkPreviewUrl)
   if (response.ok) {
-    console.log({ response })
     const jsonData = await response.json()
+    console.log({ jsonData })
     res.status(200).json(jsonData)
   } else {
-    console.log({ response })
     response = await fetch(openGraphUrl)
     const jsonData = await response.json()
+    console.log({ jsonData })
     res.status(200).json(jsonData.hybridGraph)
   }
 }
