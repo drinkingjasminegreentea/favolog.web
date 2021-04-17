678cf1a7c418ab6183430561382303067b079698
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-04-17 11:11:49 -0500

    small update

--- pages/about.js
+++ pages/about.js
@@ -5,19 +5,20 @@ export default function Page() {
       <p>
         Our mission is to help people discover the best products in the market
         through the word of mouth. We also want to give each person a chance to
-        introduce themselves to their own network of friends and family by
-        displaying their own unique favorites and preferences. And lastly, we
-        want to serve small businesses that make great products but have no
-        money to buy advertisement. We think this will be a great platform for
-        people like you to spread the good word about them. We hope you get the
-        best use out of using our free service.
+        introduce themselves to their own network of friends, family and
+        followers by displaying their own unique favorites and preferences. And
+        lastly, we want to serve small businesses that make great products, but
+        are unable to afford advertisement. We think this will be a great
+        platform for people like you to spread the good word about such
+        businesses. We hope that you can get the best use and value from using
+        our service.
       </p>
       <h3>Team</h3>
       <p>
         Favolog is a love child of Raihan and Aigerim - two mothers who are
         always striving to make the world a better place. We are located in US
         and Canada and spend our days working tirelessly to ensure Favolog is
-        doing a top notch job at bringing people value. If you have any
+        doing a top notch job at bringing people the most value. If you have any
         suggestions, please send them our way at feedback@favolog.com. We are
         always happy to hear from you.
       </p>
@@ -26,9 +27,9 @@ export default function Page() {
         You have favorite pair of jeans or a t-shirt you think everyone should
         check out. Great! The easiest way to add it to your profile is by using
         a link to the item on the internet. The system will automatically
-        extract the proper title of the item, as well as the image with the
-        source information. You can also add it manually by entering the title
-        and the image instead of the link.
+        extract the proper title, as well as the image of the item with the
+        source information. You can also add it manually by entering the
+        information manually instead of using a link.
       </p>
     </div>
   )
