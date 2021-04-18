b2364bbfd7c009350861ced98cb5ee36f17b3a0c
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-04-18 07:54:48 -0500

    quick update

--- pages/about.js
+++ pages/about.js
@@ -20,10 +20,10 @@ export default function Page() {
       </p>
       <h3>Team</h3>
       <p>
-        Favolog is a love child of Raihan and Aigerim - two mothers who are
-        always striving to make the world a better place. We are located in US
-        and Canada and spend our days working tirelessly to ensure Favolog is
-        doing a top notch job at bringing people the most value. If you have any
+        Favolog is created by Raihan and Aigerim - two mothers who are always
+        striving to make the world a better place. We are located in US and
+        Canada and spend our days working tirelessly to ensure Favolog is doing
+        a top notch job at bringing people the most value. If you have any
         suggestions, please send them our way at feedback@favolog.com. We are
         always happy to hear from you.
       </p>
